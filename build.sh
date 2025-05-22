f74cf33ffd0e67af10690e3a459d2a4652f7c4f6
Xavier Roumegue <xavier.roumegue@nxp.com>
2025-05-22 10:29:53 +0200

    build.sh: Export user git config file if existing
    Signed-off-by: Xavier Roumegue <xavier.roumegue@nxp.com>

--- build.sh
+++ build.sh
@@ -9,5 +9,12 @@ kasdir=${rootdir}/kas
 export KAS_CONTAINER_IMAGE='docker.io/xroumegue/kas-xilinx:4.6'
 export KAS_CONTAINER=${kasdir}/kas-container
 
+gitconfig_file=${HOME}/.gitconfig
+
+if [ -e "${gitconfig_file}" ];
+then
+  export GITCONFIG_FILE=${gitconfig_file}
+fi
+
 "${builderdir}"/bin/build.sh "$@"
 
